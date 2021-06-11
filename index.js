4d66e7c77f999bc8b954d3398655f170822f58fb
AliQasimzade <qasimzadeali4@gmail.com>
2021-06-11 18:07:18 +0400

    heroku fuc

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/users", (req, res) => {
 
   db.collection("data")
     .doc("6IWWfXYYe2jqsUCvQInA")
-    .update({"Lists":data})
+    .set({data},{merge:true})
 });
 
 app.listen(port, () => {
