275bccdd148feda73aec1a8885abb01414e92a84
AliQasimzade <qasimzadeali4@gmail.com>
2021-06-11 18:19:15 +0400

    fix

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/users", (req, res) => {
 
   db.collection("data")
     .doc("6IWWfXYYe2jqsUCvQInA")
-    .set({Lists},{merge:true})
+    .set(data,{merge:true})
 });
 
 app.listen(port, () => {
