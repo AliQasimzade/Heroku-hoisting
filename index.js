4d7a0a050dfba34f18ee80919cfdb0fd019c8b63
AliQasimzade <qasimzadeali4@gmail.com>
2021-06-11 15:40:21 +0400

    Added post request

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("firebase/firestore");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 var cors = require("cors");
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 var firebaseConfig = {
   apiKey: "AIzaSyBwE6VsuDodrixBsWn0nuAvBMAELAzR3b0",
@@ -30,6 +30,18 @@ app.get("/getData", (req, res) => {
       res.header("Access-Control-Allow-Origin","*");
       res.send(result.data());
     });
+});
+
+app.post("/users", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send({
+    success: "200 response",
+    res: "You are now just talked with server",
+  });
+  db.collection("data")
+    .doc("XLxHb8O2A3EY6IWWfXYYe2jqsUCvQInAa2YAt5Jq")
+    .update({ ...Lists.push(firebase.firestore.FieldValue.delete())  });
 });
 
 app.listen(port, () => {
