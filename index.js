21a3c77fe6f69e58752535a9e627b5be4789c72f
AliQasimzade <qasimzadeali4@gmail.com>
2021-07-03 11:34:24 +0400

    fixed

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("/getData", (req, res) => {
 app.post("/chart", (req, res) => {
  
   database.ref("Sidebar").on("value", function (snapshot) {
-    let table = snapshot.slice(0, req.index)
+    let table = snapshot.val().slice(0, req.index)
     res.send(table)
   });
 });
