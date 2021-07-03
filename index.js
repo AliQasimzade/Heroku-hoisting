96f1cc0c49f5c064762083386cbc748a2e1938c0
AliQasimzade <qasimzadeali4@gmail.com>
2021-07-03 13:28:26 +0400

    fixed

--- index.js
+++ index.js
@@ -39,22 +39,18 @@ app.get("/getData", (req, res) => {
 });
 
 app.post("/chart", (req, res) => {
- 
-  database.ref("Table").on("value", (snap => {
-    let table = []
-    snap.forEach(function(childsnap) {
-      let allId = Number(childsnap.key)
-     if(allId < req.body.index){
-      table.push(childsnap.val())
-     }
-
-    })
+  database.ref("Table").on("value", (snap) => {
+    let table = [];
+    snap.forEach(function (childsnap) {
+      let allId = Number(childsnap.key);
+      if (allId < req.body.index) {
+        table.push(childsnap.val());
+      }
+    });
     setTimeout(() => {
-      res.send(table)
-    },200)
-  }))
-
- 
+      res.send(table,req.body);
+    }, 200);
+  });
 });
 
 app.post("/users", (req, res) => {
