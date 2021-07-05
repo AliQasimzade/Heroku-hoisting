685c3cebf6623c0f532616a74c05a73f6ce60049
AliQasimzade <qasimzadeali4@gmail.com>
2021-07-05 15:03:20 +0400

    fixed

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post("/chart", (req, res) => {
 
 app.post("/update", (req, res) => {
   const data = req.body;
-  database.ref("Table/" + data.id).update(data);
+  // database.ref("Table/" + data.id).update(data);
   res.send(data);
 });
 
