88b0bf57c6719f90cc674b8fcdc6ae7aa6ba1422
AliQasimzade <qasimzadeali4@gmail.com>
2021-07-07 12:21:19 +0400

    fix

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post("/update", (req, res) => {
     });
   });
   let result = [...keys, keys.length + 1]
-  let newPostKey = result[length]
+  let newPostKey = result[result.length]
   // database.ref("Table/" + newPostKey).update({
   //   name: data.name,
   //   email: data.email,
